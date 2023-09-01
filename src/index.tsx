ee4f432dc207c7eeaca359a315fdfe3f5b911d1c
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-01 13:56:18 -0500

    auth0 integrated and working

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+if (
+    !process.env.REACT_APP_AUTH0_DOMAIN ||
+    !process.env.REACT_APP_AUTH0_CLIENT_ID ||
+    !process.env.REACT_APP_AUTH0_CALLBACK_URL
+) {
+    throw new Error("One or more required environment variables are missing.");
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Auth0Provider
+              domain={process.env.REACT_APP_AUTH0_DOMAIN}
+              clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+              authorizationParams={{
+                  redirect_uri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+              }}
+              >
+              <App />
+          </Auth0Provider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
