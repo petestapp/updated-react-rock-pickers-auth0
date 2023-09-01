9839cb2d7be6aa1eafb2b841469e78dd4a435adc
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-01 10:14:49 -0500

    profile page renders the profile when user is authenticated test created

--- src/tests/ProfilePage/ProfilePage.test.tsx
+++ src/tests/ProfilePage/ProfilePage.test.tsx
@@ -8,13 +8,32 @@ jest.mock("@auth0/auth0-react");
 describe("ProfilePage", () => {
     it("does not render profile information when user is not authenticated", () => {
         // arrange
-        (useAuth0 as jest.Mock).mockReturnValueOnce({ user: null });
+        (useAuth0 as jest.Mock).mockReturnValueOnce( { user: null } );
 
         // act
-       render(<ProfilePage />);
+        render(<ProfilePage />);
+
+        const profileHeading = screen.queryByText("Profile Page");
 
         // assert
-        const profilePageText = screen.queryByText("Profile Page");
-        expect(profilePageText).toBeNull();
+        expect(profileHeading).toBeNull();
+    })
+
+    it("renders the profile when user is authenticated", () => {
+        // arrange
+        (useAuth0 as jest.Mock).mockReturnValueOnce( { user: { email: "test@example.com", picture: "user-picture-url" } } );
+
+        // act
+        render(<ProfilePage />);
+
+        const profileHeading = screen.queryByText("Profile Page");
+        const userEmail = screen.queryByText("test@example.com");
+        const userPicture = screen.queryByAltText("user icon consisting of first two letters of name");
+
+        // expect
+        expect(profileHeading).toBeInTheDocument();
+        expect(userEmail).toBeInTheDocument();
+        expect(userPicture).toHaveAttribute("src", "user-picture-url");
+        expect(userPicture).toBeInTheDocument();
     })
 })
