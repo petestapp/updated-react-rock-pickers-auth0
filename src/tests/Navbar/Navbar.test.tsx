bd3b13ea193dd2986f3cc4c53ea213ffcfd60d9c
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-01 13:46:03 -0500

    navbar renders signup and login buttons when not authenticated test created

---
+++ src/tests/Navbar/Navbar.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navbar } from "../../components/Navbar/Navbar";
+
+jest.mock("@auth0/auth0-react");
+
+describe("Navbar", () => {
+    it("renders SignUp and Login buttons when not authenticated", () => {
+        // arrange
+        (useAuth0 as jest.Mock).mockReturnValueOnce( { isAuthenticated: false } );
+
+        // act
+        render(<Navbar />);
+
+        const signUpButton = screen.getByText("Sign Up");
+        const loginButton = screen.getByText("Log In");
+
+        // assert
+        expect(signUpButton).toBeInTheDocument();
+        expect(loginButton).toBeInTheDocument();
+    })
+})
