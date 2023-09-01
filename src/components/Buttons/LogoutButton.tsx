350e21460e365a1cce0aa85969d8776e1cbf683c
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-01 11:37:19 -0500

    logout button tests passing

--- src/components/Buttons/LogoutButton.tsx
+++ src/components/Buttons/LogoutButton.tsx
@@ -1,4 +1,22 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const LogoutButton: React.FC = () => {
+    const { logout } = useAuth0();
+
+    const handleLogout = () => {
+        logout( {
+            logoutParams: {
+                logoutParams: {
+                    returnTo: "http://localhost:3000/"
+                }
+            }
+        } );
+    }
+
+    return (
+        <button onClick={handleLogout}>
+            Log Out
+        </button>
+    )
 }
