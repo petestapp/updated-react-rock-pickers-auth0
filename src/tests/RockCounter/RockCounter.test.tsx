f7a102364f4316d7c624eee60cafb10f57576bf3
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-05 11:42:44 -0500

    rock counter renders rock counter component test created

--- src/tests/RockCounter/RockCounter.test.tsx
+++ src/tests/RockCounter/RockCounter.test.tsx
@@ -1,6 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { RockCounter } from "../../components/RockCounter/RockCounter";
 
 describe("RockCounter", () => {
+    it("renders RockCounter component", () => {
+        // arrange and act
+        render(<RockCounter />);
+
+        const rocksPicked = screen.getByText("Rocks Picked: 0");
+        const increaseButton = screen.getByText("Increase");
+        const decreaseButton = screen.getByText("Decrease");
+        const resetButton = screen.getByText("Reset");
+
+        // assert
+        expect(rocksPicked).toBeInTheDocument();
+        expect(increaseButton).toBeInTheDocument();
+        expect(decreaseButton).toBeInTheDocument();
+        expect(resetButton).toBeInTheDocument();
+    })
 })
