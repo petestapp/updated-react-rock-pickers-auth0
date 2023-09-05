9a7b28d321e24ae8236b0c173b2e06108b27e1e7
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-05 14:57:26 -0500

    rock counter tests passing

--- src/components/RockCounter/RockCounter.tsx
+++ src/components/RockCounter/RockCounter.tsx
@@ -1,4 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const RockCounter: React.FC = () => {
+    const [rocks, setRocks] = useState(0);
+    let isDone = false;
+
+    const increaseCount = () => {
+        setRocks(rocks + 1);
+    }
+
+    const decreaseCount = () => {
+        rocks < 1 ? setRocks(0) : setRocks(rocks - 1);
+    }
+
+    const resetCount = () => {
+        setRocks(0);
+    }
+
+    const checkIfDone = () => {
+        if (rocks >= 10) {
+            isDone = true;
+        } else {
+            isDone = false;
+        }
+    }
+
+    checkIfDone();
+
+    return (
+        <div>
+            <div>Rocks Picked: {rocks}</div>
+            <div>
+                <button onClick={increaseCount}>Increase</button>
+                <button onClick={decreaseCount}>Decrease</button>
+                <button onClick={resetCount}>Reset</button>
+                <p>{isDone ? "Done" : "Not Done"}</p>
+            </div>
+        </div>
+    )
 }
