d5ea98de6a35c9b5193eb67dbcf1973280bfb6e4
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-05 15:29:08 -0500

    rock counters added to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
-import {Navbar} from "../Navbar/Navbar";
-import {ProfilePage} from "../ProfilePage/ProfilePage";
+import { Navbar } from "../Navbar/Navbar";
+import { ProfilePage } from "../ProfilePage/ProfilePage";
+import { RockCounter } from "../RockCounter/RockCounter";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <header>
-        <h1>React Rock Pickers</h1>
-      </header>
-      <ProfilePage />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <header>
+                <h1>React Rock Pickers</h1>
+            </header>
+            <ProfilePage/>
+            <h2>Luke</h2>
+            <RockCounter />
+            <h2>JJ</h2>
+            <RockCounter />
+            <h2>Sam</h2>
+            <RockCounter />
+            <h2>Pete</h2>
+            <RockCounter />
+        </div>
+    );
 }
 
 export default App;
