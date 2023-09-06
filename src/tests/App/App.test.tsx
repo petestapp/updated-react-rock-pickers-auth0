7ce52c9ebea520c94a5bdcd201f5d90d63fe0f26
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-06 14:52:33 -0500

    navbar tests passing; app.test.tsx indenting fixed

--- src/tests/App/App.test.tsx
+++ src/tests/App/App.test.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from '../../components/App/App';
-import { useAuth0, Auth0ContextInterface } from "@auth0/auth0-react";
+import {useAuth0, Auth0ContextInterface} from "@auth0/auth0-react";
 
 jest.mock("@auth0/auth0-react");
 
 const mockedUseAuth0 = useAuth0 as jest.MockedFunction<typeof useAuth0>
 describe("App", () => {
-  beforeEach(() => {
-    mockedUseAuth0.mockReturnValue({
-      isAuthenticated: true,
-      user: {
-        name: 'Test User',
-        email: 'test.user@email.com',
-      },
-      getAccessTokenSilently: jest.fn().mockResolvedValue('access_token'),
-      getAccessTokenWithPopup: jest.fn(),
-      getIdTokenClaims: jest.fn(),
-      loginWithRedirect: jest.fn(),
-      loginWithPopup: jest.fn(),
-      logout: jest.fn(),
-      isLoading: false,
-      error: undefined,
-    } as unknown as Auth0ContextInterface);
-  });
+    beforeEach(() => {
+        mockedUseAuth0.mockReturnValue({
+            isAuthenticated: true,
+            user: {
+                name: 'Test User',
+                email: 'test.user@email.com',
+            },
+            getAccessTokenSilently: jest.fn().mockResolvedValue('access_token'),
+            getAccessTokenWithPopup: jest.fn(),
+            getIdTokenClaims: jest.fn(),
+            loginWithRedirect: jest.fn(),
+            loginWithPopup: jest.fn(),
+            logout: jest.fn(),
+            isLoading: false,
+            error: undefined,
+        } as unknown as Auth0ContextInterface);
+    });
 
-  it('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/React Rock Pickers/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+    it('renders learn react link', () => {
+        render(<App/>);
+        const linkElement = screen.getByText(/React Rock Pickers/i);
+        expect(linkElement).toBeInTheDocument();
+    });
 })
--- src/tests/Navbar/Navbar.test.tsx
+++ src/tests/Navbar/Navbar.test.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { useAuth0 } from "@auth0/auth0-react";
+import {Auth0ContextInterface, useAuth0} from "@auth0/auth0-react";
 import { Navbar } from "../../components/Navbar/Navbar";
 
 jest.mock("@auth0/auth0-react");
 
+const mockedUseAuth0 = useAuth0 as jest.MockedFunction<typeof useAuth0>
+
 describe("Navbar", () => {
+    beforeEach(() => {
+        mockedUseAuth0.mockReturnValue({
+            user: {
+                name: 'Test User',
+                email: 'test.user@email.com',
+            },
+            getAccessTokenSilently: jest.fn().mockResolvedValue('access_token'),
+            getAccessTokenWithPopup: jest.fn(),
+            getIdTokenClaims: jest.fn(),
+            loginWithRedirect: jest.fn(),
+            loginWithPopup: jest.fn(),
+            logout: jest.fn(),
+            isLoading: false,
+            error: undefined,
+        } as unknown as Auth0ContextInterface);
+    });
+
     it("renders SignUp and Login buttons when not authenticated", () => {
         // arrange
         (useAuth0 as jest.Mock).mockReturnValueOnce( { isAuthenticated: false } );
@@ -28,7 +47,7 @@ describe("Navbar", () => {
         // act
         render(<Navbar />);
 
-        const logOutButton = screen.getByText("Log out");
+        const logOutButton = screen.getByText("Log Out");
 
         // assert
         expect(logOutButton).toBeInTheDocument();
