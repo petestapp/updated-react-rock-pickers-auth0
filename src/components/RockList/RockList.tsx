6896dc6f22b06caa4cb15a87b8e984714c36a882
petestapp <63821853+petestapp@users.noreply.github.com>
2023-09-07 19:00:13 -0500

    rocklist displaying different lists for different roles

--- src/components/RockList/RockList.tsx
+++ src/components/RockList/RockList.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
+import jwtDecode, {JwtPayload} from "jwt-decode";
 
 interface Rock {
     id: number,
     type: string
+}
+
+interface jwtClaims extends JwtPayload {
+    permissions: string[];
 }
 
 export const RockList: React.FC = () => {
@@ -18,12 +23,26 @@ export const RockList: React.FC = () => {
             try {
                 const token = await getAccessTokenSilently();
                 console.log(token);
-                const response = await axios.get<Rock[]>("http://localhost:6060/getAllRocks", {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    }
-                });
-                setRockListArray(response.data);
+                const decodedToken = jwtDecode<jwtClaims>(token);
+                console.log(decodedToken);
+                const permissions = decodedToken.permissions;
+                console.log(permissions);
+
+                if (permissions.includes("adminlist")) {
+                    const response = await axios.get<Rock[]>("http://localhost:6060/getAllRocks", {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        }
+                    })
+                    setRockListArray(response.data);
+                } else if (permissions.includes("userlist")) {
+                    const response = await axios.get<Rock[]>("http://localhost:6060/getAllRocksForUser", {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        }
+                    })
+                    setRockListArray(response.data);
+                }
             } catch (error) {
                 console.error("error getting all rocks", error);
             }
